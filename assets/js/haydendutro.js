4094f53785ce55a8084b0853e3aa570043bc3781
HaydenDutro <dutrohn@miamioh.edu>
2022-06-11 14:14:21 +0300

    Update haydendutro.js

--- assets/js/haydendutro.js
+++ assets/js/haydendutro.js
@@ -1,4 +1,4 @@
-import Typewriter from '/node_modules/typewriter-effect/dist/core';
+import Typewriter from '/node_modules/typewriter-effect/dist/core.js"';
 
 function homeType() {
     let welcomeHead = document.getElementById("test123");
