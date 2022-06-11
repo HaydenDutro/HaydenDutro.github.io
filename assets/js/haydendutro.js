a202534e9b158fd553aa4c6e06ea0a6d2b360af6
HaydenDutro <dutrohn@miamioh.edu>
2022-06-11 14:02:32 +0300

    test of type modual

---
+++ assets/js/haydendutro.js
@@ -0,0 +1,12 @@
+import Typewriter from 'assets/js/typewriter-effect/dist/core';
+
+function homeType() {
+    let welcomeHead = document.getElementById("test123");
+    console.log(welcomeHead);
+    welcomeHead.innerHTML ="got here";
+    console.log(welcomeHead);
+
+}
+
+
+
