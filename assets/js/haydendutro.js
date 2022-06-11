d52466bf654deb6ed3fcd96931a9f288b94d779f
HaydenDutro <dutrohn@miamioh.edu>
2022-06-11 14:49:58 +0300

    moved type-writer effect

--- assets/js/haydendutro.js
+++ assets/js/haydendutro.js
@@ -1,4 +1,4 @@
-import Typewriter from '/node_modules/typewriter-effect/dist/core.js';
+import Typewriter from '/assets/js/typewriter-effect/dist/core.js';
 
 function homeType() {
     let welcomeHead = document.getElementById("test123");
