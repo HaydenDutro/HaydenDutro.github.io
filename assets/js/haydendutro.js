68ead835447a1307c4c4bbb046913f49c968a3a0
HaydenDutro <dutrohn@miamioh.edu>
2022-07-03 20:33:53 +0300

    Made Personal Projects page

--- assets/js/haydendutro.js
+++ assets/js/haydendutro.js
@@ -1,10 +1,3 @@
-function homeType() {
-    let welcomeHead = document.getElementById("test123");
-    console.log(welcomeHead);
-    welcomeHead.innerHTML ="got here";
-    console.log(welcomeHead);
-
-}
 
 
 
